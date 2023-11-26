9050917658455269868ed7a9379c009780b5a61c
Hassan Kamran <hassankamran98@yahoo.com>
2023-11-27 00:40:59 +0300

    Implementing Query Service and Event Bus

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,19 +1,4 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-  const fetchComments = async () => {
-    const response = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(response.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, [postId]);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => (
     <li key={comment.id}>{comment.content}</li>
   ));
