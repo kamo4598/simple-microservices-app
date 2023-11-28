f0c4c9f325fe867b31dfd0ba73fe6f0ae0ee2cae
Hassan Kamran <hassankamran98@yahoo.com>
2023-11-28 18:41:06 +0300

    Moderation Service + Syncing Missed Events

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,6 +1,15 @@
 const CommentList = ({ comments }) => {
+  const getRenderedContent = (comment) => {
+    if (comment.status === "rejected") {
+      return "This comment has been rejected";
+    } else if (comment.status === "approved") {
+      return comment.content;
+    } else {
+      return "This comment is awaiting moderation";
+    }
+  };
   const renderedComments = comments.map((comment) => (
-    <li key={comment.id}>{comment.content}</li>
+    <li key={comment.id}>{getRenderedContent(comment)}</li>
   ));
 
   return (
--- event-bus/index.js
+++ event-bus/index.js
@@ -5,9 +5,11 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", async (req, res) => {
   const event = req.body;
-
+  events.push(event);
   await axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
@@ -17,8 +19,15 @@ app.post("/events", async (req, res) => {
   await axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
+  await axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
